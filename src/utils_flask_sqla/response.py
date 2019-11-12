3986e1334e3c470b647aa2a19ebd39ddd132d0c8
Joël CLEMENT <joelclems@gmail.com>
2019-11-12 22:10:52 +0100

    json_resp correction

--- src/utils_flask_sqla/response.py
+++ src/utils_flask_sqla/response.py
@@ -17,19 +17,20 @@ def json_resp_accept(accepted_list=[]):
         def _json_resp(*args, **kwargs):
             res = fn(*args, **kwargs)
             if isinstance(res, tuple):
-                res['accepted_list'] = accepted_list
-                return to_json_resp(*res)
+                return to_json_resp(*res, accepted_list=accepted_list)
             else:
-                return to_json_resp(res, accepted_list)
+                return to_json_resp(res, accepted_list=accepted_list)
 
         return _json_resp
     return json_resp
 
+
 json_resp = json_resp_accept()
 json_resp_accept_empty_list = json_resp_accept([[]])
 
+
 def to_json_resp(
-    res, status=200, filename=None, as_file=False, indent=None, extension="json", accepted_list = False
+    res, status=200, filename=None, as_file=False, indent=None, extension="json", accepted_list=[]
 ):
     if not ( res or res in accepted_list ) :
         status = 404
