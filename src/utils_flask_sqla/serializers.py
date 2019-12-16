345c1e9447c4613eaa4e451df9d4586b97e3479a
joel.clement <joelclems@gmail.com>
2019-12-16 17:35:44 +0100

    correct cond depth

--- src/utils_flask_sqla/serializers.py
+++ src/utils_flask_sqla/serializers.py
@@ -95,7 +95,7 @@ def serializable(cls):
                 liste des relationships qui doivent être prise en compte
         """
 
-        if depth >= 0:
+        if isinstance(depth, int) and depth >= 0:
             recursif = True
             depth -= 1
 
