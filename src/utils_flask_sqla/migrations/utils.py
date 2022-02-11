91203f625406a7ee56cd580155c251ed43825578
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-02-11 12:08:58 +0100

    Add data_dir params for outside context use

--- src/utils_flask_sqla/migrations/utils.py
+++ src/utils_flask_sqla/migrations/utils.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 import lzma
 import os, os.path
 
-logger = logging.getLogger('alembic.runtime.migration')
+logger = logging.getLogger("alembic.runtime.migration")
 
 """
 Ce contextmanager permet d’ouvrir un fichier après l’avoir préalablement téléchargé.
@@ -17,13 +17,18 @@ dans les autres cas le fichier est ouvert comme un fichier normal
 Le fichier téléchargé est enregistré dans le dossier spécifié par -x data-directory=…
 Si aucun dossier n’est spécifié, un dossier temporaire, supprimé à la fin de la migration, est utilisé.
 """
+
+
 class open_remote_file(ExitStack):
-    def __init__(self, base_url, filename, open_fct=lzma.open):
+    def __init__(self, base_url, filename, open_fct=lzma.open, data_dir=None):
         super().__init__()
         self.base_url = base_url
         self.filename = filename
         self.open_fct = open_fct
-        self.data_dir = context.get_x_argument(as_dictionary=True).get('data-directory')
+        if not data_dir:
+            self.data_dir = context.get_x_argument(as_dictionary=True).get("data-directory")
+        else:
+            self.data_dir = data_dir
 
     def __enter__(self):
         stack = super().__enter__()
@@ -35,7 +40,8 @@ class open_remote_file(ExitStack):
         remote_file_path = os.path.join(self.data_dir, self.filename)
         if not os.path.isfile(remote_file_path):
             logger.info("Downloading '{}'…".format(self.filename))
-            with urlopen('{}{}'.format(self.base_url, self.filename)) as response, \
-                                              open(remote_file_path, 'wb') as remote_file:
+            with urlopen("{}{}".format(self.base_url, self.filename)) as response, open(
+                remote_file_path, "wb"
+            ) as remote_file:
                 copyfileobj(response, remote_file)
         return stack.enter_context(self.open_fct(remote_file_path))
