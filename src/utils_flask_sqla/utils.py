452e03290288c26db9b2f144dbb34f3c7bf2b3c9
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2023-11-02 09:46:50 +0100

    add join check function

--- src/utils_flask_sqla/utils.py
+++ src/utils_flask_sqla/utils.py
@@ -5,6 +5,8 @@ from contextlib import ExitStack
 from tempfile import TemporaryDirectory
 from shutil import copyfileobj
 from urllib.request import urlopen
+from contextlib import suppress
+from sqlalchemy.sql import visitors
 
 
 class remote_file(ExitStack):
@@ -32,3 +34,33 @@ class remote_file(ExitStack):
             with urlopen(self.url) as response, open(remote_file_path, "wb") as remote_file:
                 copyfileobj(response, remote_file)
         return remote_file_path
+
+def is_already_joined(my_class, query):
+    """
+    Check if the given class is already present is the current query
+    _class: SQLAlchemy class
+    query: SQLAlchemy query
+    return boolean
+    """
+    for visitor in visitors.iterate(query.statement):
+        # Checking for `.join(Parent.child)` clauses
+        if visitor.__visit_name__ == 'binary':
+            for vis in visitors.iterate(visitor):
+                # Visitor might not have table attribute
+                with suppress(AttributeError):
+                    # Verify if already present based on table name
+                    if my_class.__table__.fullname == vis.table.fullname:
+                        return True
+        # Checking for `.join(Child)` clauses
+        if visitor.__visit_name__ == 'table':
+            # Visitor might be of ColumnCollection or so, 
+            # which cannot be compared to model
+            with suppress(TypeError):
+                if my_class == visitor.entity_namespace:
+                    return True
+        # Checking for `Model.column` clauses
+        if visitor.__visit_name__ == "column":
+            with suppress(AttributeError):
+                if my_class.__table__.fullname == visitor.table.fullname:
+                    return True
+    return False
