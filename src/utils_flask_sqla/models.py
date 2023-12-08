08d81340fcb7067b6eb0b04c4a8b66ef572f34c5
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2023-12-08 13:35:01 +0100

    Delete CustomSelect, Add qfilter decorator to replace queryclass

--- src/utils_flask_sqla/models.py
+++ src/utils_flask_sqla/models.py
@@ -1,15 +1,88 @@
-from .sqlalchemy import CustomSelect
-from flask_sqlalchemy.model import Model
+from sqlalchemy.sql.expression import BooleanClauseList, BinaryExpression
+from flask_sqlalchemy.model import DefaultMeta
+from sqlalchemy.sql import select, Select
 
 
-class SelectModel(Model):
-    __abstract__ = True
+def qfilter(*args_dec, **kwargs_dec):
+    """
+    This decorator allows you to constrain a SQLAlchemy model method to return a whereclause (by default) or a query. If
+    its return_query is set to True and no query is given in a `query` parameter, it will create one with a simple select: `select(model)`. The latter
+    is accessible through `kwargs.get("query")` in the decorated method.
 
-    @classmethod
-    @property
-    def select(cls):
-        if hasattr(cls, "__select_class__"):
-            select_cls = cls.__select_class__
-        else:
-            select_cls = CustomSelect
-        return select_cls._create_future_select(cls)  # SQLA 2.0: _create_future_select → _create
+
+    The decorated query requires the following minimum parameters (cls,**kwargs).
+
+    >>> from utils_flask_sqla.models import qfilter
+    >>> from sqlalchemy.sql import select
+    >>> class Station(NomenclaturesMixin, db.Model):
+            __tablename__ = "t_stations"
+            __table_args__ = {"schema": "pr_occhab"}
+            # If you wish the method to return a whereclause
+            @qfilter
+            def filter_by_params(cls,**kwargs):
+                query = kwargs("query") #  select(Station)
+                if "id_station" in kwargs:
+                    query = query.filter_by(id_station=kwargs["id_station"])
+                return query.whereclause
+            # If you wish the method to return a query
+            @qfilter(return_query=True)
+            def filter_by_paramsQ(cls,**kwargs):
+                query = kwargs("query") #  select(Station)
+                if "id_station" in kwargs:
+                    query = query.filter_by(id_station=kwargs["id_station"])
+                return query
+
+    >>> query = Station.filter_by_paramsQ(id_station=1)
+    >>> query2 = select(Station).where(Station.filter_by_params(id_station=1))
+
+    Returns
+    -------
+    function
+        decorated method
+
+    Raises
+    ------
+    ValueError
+        Method's class is not DefaultMeta class
+    ValueError
+        if return_query is True and return value of the decorated method is not Select
+    ValueError
+        if return_query is False and return value of the decorated method is not a : `bool` or sqlalchemy.sql.expression.BooleanClauseList` or `sqlalchemy.sql.expression.BinaryExpression`
+    """
+    is_query = kwargs_dec.get("return_query", False)
+
+    def _qfilter(method):
+        def _(*args, **kwargs):
+            # verify if class of the method is ORM model
+            sqla_class = args[0]
+            if not isinstance(sqla_class, DefaultMeta):
+                raise ValueError(
+                    "The decorated method's class must inherit from flask_sqlalchemy.model.DefaultMeta"
+                )
+
+            query = kwargs.get("query", None)
+
+            # if no query given
+            if query == None:
+                query = select(sqla_class)
+                kwargs["query"] = query
+            result = method(*args, **kwargs)
+
+            if is_query and not isinstance(result, Select):
+                raise ValueError("Your method must return a SQLAlchemy Select object ")
+
+            authorise_whereclause_type = [bool, BooleanClauseList, BinaryExpression]
+            if not is_query and not any(
+                [isinstance(result, type_) for type_ in authorise_whereclause_type]
+            ):
+                raise ValueError(
+                    "Your method must return an object in the following types: {} ".format(
+                        ", ".join(map(lambda cls: cls.__name__, authorise_whereclause_type))
+                    )
+                )
+            # if filter is wanted as where clause
+            return result
+
+        return classmethod(_)
+
+    return _qfilter
