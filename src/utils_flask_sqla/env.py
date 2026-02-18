5c9f4197855884610d4d6dbb0c5034008a9fff84
Amandine <amandine.sahl@cevennes-parcnational.fr>
2026-02-18 14:02:08 +0100

    Feat/use db env (#61)
    * create env file to store db
    
    * Use sa.Table and env db
    
    ---------
    
    Co-authored-by: TheoLechemia <lechemia.theo@gmail.com>

---
+++ src/utils_flask_sqla/env.py
@@ -0,0 +1,10 @@
+from os import environ
+from importlib import import_module
+
+
+db_path = environ.get("FLASK_SQLALCHEMY_DB")
+if not db_path:
+    raise Exception("FLASK_SQLALCHEMY_DB env var is missing")
+db_module_name, db_object_name = db_path.rsplit(".", 1)
+db_module = import_module(db_module_name)
+db = getattr(db_module, db_object_name)
